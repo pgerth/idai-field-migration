6dc7f65d972041b487f03141baf17996795e6ddf
phg <philipp.gerth@gmx.net>
2017-02-21 15:14:50 +0100

    Added GeoJSON file support.

--- migration.py
+++ migration.py
@@ -2,17 +2,36 @@
 
 import xmltodict
 import json
+from codecs import open
 
 mapping = {
     'PS_AusgrabungID':'identifier',
     'KurzbeschreibungAusgrabung':'shortDescription',
     'DS_Bearbeiter':'eventMetadataRevisor',
     'DS_Erstellungsdatum':'eventMetadataCreationDate',
+    'Grabungsverlauf':'longDescription',
+    'Auto_Ausgrabungkennung':'number',
+    'Schnittleiter':'processor',
+    'Sonstige_Anmerkung':'notes',
+    'Grabungsumfang':'classification',
+    'PS_BefundID':'relations',
     }
 
 print('Read external xml data into a dictionary')
-with open('Pergamon_Ausgrabung.xml') as fd:
-    xml = xmltodict.parse(fd.read())
+with open('Pergamon_Ausgrabung.xml', 'r', 'utf-8') as fd:
+    xml = xmltodict.parse(fd.read(), encoding='utf-8')
+
+# loads a geojson file with the geometries of the migrated objects
+with open('Pergamon_Ausgrabung.geojson') as f: 
+    jsonFile = json.load(f)
+features = jsonFile['features']
+
+# Function which searches for a geometry of a specific object, 
+# identified by the iDAIfield Seriennummer
+def getGeom(serialnb):
+    for feature in features:
+        if int(serialnb) == int(feature['properties']['id']):
+            return feature['geometry']
 
 trenches = xml['FMPDSORESULT']['ROW']
 error_list = []
@@ -31,9 +50,10 @@ for trench in trenches:
         else:
             del trench[key]
     trench['type'] = 'trench'
+    trench['geometries'] = getGeom(trench['identifier'])
 
 print('Create JSONL as external file')
-with open('Pergamon_Ausgrabung.jsonl', 'w') as outfile:
+with open('Pergamon_Ausgrabung.jsonl', 'w', 'utf-8') as outfile:
     for entry in trenches:
         json.dump(entry, outfile)
         outfile.write('\n')
