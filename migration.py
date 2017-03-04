3b93ecec8dba0ee4ab0fc177f6015e27292608eb
phg <philipp.gerth@gmx.net>
2017-03-04 20:53:36 +0100

    support for relations

--- migration.py
+++ migration.py
@@ -4,25 +4,68 @@ import xmltodict
 import json
 from codecs import open
 
-mapping = {
+attrMapping = {
+    # Befund
+    'PS_BefundID':'identifier',
+    'Auto_BefundKennung':'number',
+    'KurzbeschreibungBefund':'shortDescription',
+    'Schicht_Beschreibung':'longDescription',
+    'Mauer_Beschreibung':'longDescription',
+    'Masze_Laenge':'length',
+    'Masze_Hoehe':'height',
+    'Masze_Breite':'width',
+    'Befundart':'classificaton',
+    'Tagebuch':'diary',
+    'Datum_Abtragung':'endingDate',
+    'DS_Bearbeiter':'processor',
+}
+
+Keramik = {
+    # Keramik
+    'PS_KeramikID':'identifier',
+    'Auto_Objektkennung_Index':'number',
+    'Materialklasse':'shortDescription',
+    'Nummer_Fund':'number',
+    'Durchmesser':'diameter',
+    'Herkunft':'provenance',
+    'Hoehe':'height',
+    'Laenge':'length',
+    'Gewicht':'weight',
+    'Aufbewahrung_Ort':'inventoryLocation',
+    'Aufbewahrung_InvNr':'inventoryNumber',
+    'Erhaltung_Sonstiges':'condition',
+    'Funddatum':'date',
+    'Grobdatierung':'dating',
+}
+
+Ausgrabung = {
+    # Ausgrabung
     'PS_AusgrabungID':'identifier',
-    'KurzbeschreibungAusgrabung':'shortDescription',
-    'DS_Bearbeiter':'eventMetadataRevisor',
-    'DS_Erstellungsdatum':'eventMetadataCreationDate',
-    'Grabungsverlauf':'longDescription',
     'Auto_Ausgrabungkennung':'number',
+    'KurzbeschreibungAusgrabung':'shortDescription',
+    'Grabungsverlauf':'longDescription',
     'Schnittleiter':'processor',
     'Sonstige_Anmerkung':'notes',
-    'Grabungsumfang':'classification',
-    'PS_BefundID':'relations',
     }
 
+relationMapping = {
+#    'FS_BefundID':'isLocatedIn',
+    'FS_AusgrabungID':'belongsTo',
+#    'PS_BefundID':'includes',
+    'PS_KeramikID':'embeds',
+}
+
+idaifieldFile = 'Pergamon_Befund.xml'
+geometryFile = 'Pergamon_Befund.geojson'
+outputFile = 'Pergamon_Befund.jsonl'
+datasetType = 'context'
+
 print('Read external xml data into a dictionary')
-with open('Pergamon_Ausgrabung.xml', 'r', 'utf-8') as fd:
+with open(idaifieldFile, 'r', 'utf-8') as fd:
     xml = xmltodict.parse(fd.read(), encoding='utf-8')
 
 # loads a geojson file with the geometries of the migrated objects
-with open('Pergamon_Ausgrabung.geojson') as f: 
+with open(geometryFile) as f: 
     jsonFile = json.load(f)
 features = jsonFile['features']
 
@@ -33,28 +76,36 @@ def getGeom(serialnb):
         if int(serialnb) == int(feature['properties']['id']):
             return feature['geometry']
 
-trenches = xml['FMPDSORESULT']['ROW']
+datasets = xml['FMPDSORESULT']['ROW']
 error_list = []
-for key, value in trenches[1].items():
-    if key not in mapping.keys():
+for key, value in datasets[1].items():
+    if key not in attrMapping.keys():
         error_list.append(key)
 
 print('Change values for keys in dictionary')
-for trench in trenches:
-    for key, value in trench.items():
+for dataset in datasets:
+    for key, value in dataset.items(): 
         if value is None:
-            del trench[key]
-        elif key in mapping.keys():
-            trench[mapping[key]] = trench[key]
-            del trench[key]
+            del dataset[key]
+        elif key in relationMapping.keys():
+            if dataset.get('relations') is None:
+                dataset['relations'] = {}
+            if type(dataset[key]) is unicode:
+                dataset['relations'][relationMapping[key]] = dataset[key]
+            else: 
+                dataset['relations'][relationMapping[key]] = dataset[key]['DATA']
+            del dataset[key]
+        elif key in attrMapping.keys():
+            dataset[attrMapping[key]] = dataset[key]
+            del dataset[key]
         else:
-            del trench[key]
-    trench['type'] = 'trench'
-    trench['geometries'] = getGeom(trench['identifier'])
+            del dataset[key]
+    dataset['type'] = datasetType
+    dataset['geometries'] = [getGeom(dataset['identifier'])]
 
 print('Create JSONL as external file')
-with open('Pergamon_Ausgrabung.jsonl', 'w', 'utf-8') as outfile:
-    for entry in trenches:
+with open(outputFile, 'w', 'utf-8') as outfile:
+    for entry in datasets:
         json.dump(entry, outfile)
         outfile.write('\n')
 
